f11fec35a0007ed3998b49376109f59e3cbaeb88
abhishekverma108 <71845709+abhishekverma108@users.noreply.github.com>
2023-02-28 20:42:55 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -5,15 +5,17 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "bucket-for-practical-feb"
+  //by this if we destroy our terraform environment our data on s3 will not be destroy 
   lifecycle {
     prevent_destroy = false
   }
   force_destroy = true
 
 }
-/*resource "aws_s3_bucket_versioning" "versioning_example" {
+// versioning if we want to go back to older version
+resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.example.id
   versioning_configuration {
     status = "Enabled"
   }
-}*/
+}
